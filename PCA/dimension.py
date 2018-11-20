a7543c3fa67ed83bd67145dfdb875599a61ea7b3
JIN-Frank <45082261+JIN-Frank@users.noreply.github.com>
2018-11-19 23:51:18 -0800

    Add files via upload

---
+++ PCA/dimension.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 19 23:49:51 2018
+
+@author: jinqingyuan
+"""
+
+def calnum(eig_vals):
+    '''
+    Determine when new subspace could provide enough relevant information(over 85%)
+    Input:
+    eig_vals(datatype:list)
+    Output:
+    Nmin(datatype:int)
+    '''
+    assert isinstance(eig_vals,list)
+    
+    Len = len(eig_vals)
+    Pe = 0
+    Nmin = 0
+    for i in range(Len):
+        Pe = Pe + eig_vals[i]
+        Ppe = Pe/sum(eig_vals)
+        if Ppe > 0.85:
+            Nmin = i+1
+            break
+    return Nmin
