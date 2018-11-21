3e9a676611b68fdd04e6a2e209db2dda8c85c330
Weinan Li <liwn@mail.sustc.edu.cn>
2018-11-21 12:24:25 -0800

    Update Test_Regression.py

--- Regression_prediction/Test_Regression.py
+++ Regression_prediction/Test_Regression.py
@@ -1,65 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 21 12:19:09 2018
 
-# coding: utf-8
-
-# In[1]:
+@author: ericli
+"""
 
 
 import pandas
 import numpy as np
 from numpy.linalg import inv
-from sklearn import datasets, linear_model
+import torch
+from torch.autograd import Variable
+import torch .nn. functional as F
 
 
-# In[2]:
-
-
+#read data
 data=pandas.read_csv('df_NaN.csv')
-
-
-# In[3]:
-
 
 #set Developing to 1 and Developed to 0
 data['Status'][data['Status'] == 'Developing']  = 0
 data['Status'][data['Status'] == 'Developed']  = 1
 
 
-#data normalization for further processing
-#data.apply(normalization,axis=0)
-
-
-# In[4]:
-
-
 #extract target data
 
-target=data['Life expectancy ']
-target=np.array(target)
-#Ignore the country and year effect
+target=np.array(data['Life expectancy '])
+
+#Ignore the country and year 
 data=data.drop(['Country','Year','Unnamed: 0','Life expectancy '],axis=1)
-#normalize data
-
-
-# In[5]:
-
 
 #extract normalization parameters and normalize original data
 dataMax=np.array(data.max())
 dataMin=np.array(data.min())
 dataMean=np.array(data.mean())
 data=data.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
-# is it necessary to normalize label data?
+# is it necessary to normalize label data? seems not
 life=target.copy()
 #target=(target-target.min())/(target.max()-target.min())
 
-
-# In[6]:
-
-
 train_x=np.array(data)
-
-
-# In[29]:
 
 
 def least_square(train_x, train_y):
@@ -73,37 +52,14 @@ def least_square(train_x, train_y):
     return weights.astype(float)
 
 least_square_weights=least_square(train_x,target)
-#prove to be very inaccurate
-
-
-# In[30]:
-
+#not very accurate
 
 prediction=np.dot(train_x,least_square_weights)
-
-
-# In[31]:
-
-
-prediction
-
-
-# In[32]:
-
-
 MSE=np.sum((target-prediction)**2)/prediction.size
-
-
-# In[33]:
-
-
-MSE
-
-
-# In[8]:
-
+print('MSE of leas square method',MSE)
 
 """
+#not working, need to debug later
 def gradient_descent(train_x, train_y, maxCycle, alpha):
      
     numSamples, numFeatures = np.shape(train_x)
@@ -118,24 +74,19 @@ def gradient_descent(train_x, train_y, maxCycle, alpha):
 """
 
 
-# In[9]:
+#train a shallow neural network with pytorch, or linear regression with pytorch
+#convert numpy data to torch variables
+#use GPU if avaiable
+if torch.cuda.is_available():
+    torch_x=Variable(torch.from_numpy(train_x.astype(float)).cuda(),requires_grad = True)
+    torch_y=Variable(torch.from_numpy(target).cuda(),requires_grad = False)
+else:
+    torch_x=Variable(torch.from_numpy(train_x.astype(float)),requires_grad = True)
+    torch_y=Variable(torch.from_numpy(target),requires_grad = False)
 
-
-#train a shallow neural network with pytorch
-
-
-import torch
-import sys
-from torch.autograd import Variable
-
-import torch .nn. functional as F
-
-torch_x=Variable(torch.from_numpy(train_x.astype(float)).cuda(),requires_grad = True)
-torch_y=Variable(torch.from_numpy(target).cuda(),requires_grad = False)
-
-class Model(torch.nn.Module):
+class ShallowModel(torch.nn.Module):
     def __init__(self):
-        super(Model, self).__init__()
+        super(ShallowModel, self).__init__()
         self.fc1 = torch.nn.Linear(19,57).double()
         #self.fc2 = torch.nn.Linear(19,19).double()
         self.fc2 = torch.nn.Linear(57,1).double()
@@ -146,102 +97,105 @@ class Model(torch.nn.Module):
         x=self.fc2(x)
         return x
     
-model=Model().cuda()
-criterion=torch.nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-            
+class LinearModel(torch.nn.Module):
+    def __init__(self):
+        super(LinearModel, self).__init__()
+        self.fc1 = torch.nn.Linear(19,1).double()
+        #self.fc2 = torch.nn.Linear(19,19).double()
+        #self.fc2 = torch.nn.Linear(57,1).double()
+        
+    def forward(self,x):
+        #x=F.relu(self.fc1(x))
+        #x=F.relu(self.fc2(x))
+        x=self.fc1(x)
+        return x
+    
+
+def train_model(model,epochs,trainsize=0.8):
+    assert isinstance(model,str)
+    assert isinstance(epochs,int) and epochs>1
+    
+    if torch.cuda.is_available():
+        if model=='shallow':
+            model=ShallowModel().cuda()
+        elif model=='linear':
+            model=LinearModel().cuda()
+    else:
+        if model=='shallow':
+            model=ShallowModel()
+        elif model=='linear':
+            model=LinearModel()
+    
+    criterion=torch.nn.MSELoss()
+    optimizer= torch.optim.SGD(model.parameters(), lr=0.01)
+    
+    #separate trainning set and testset
+    #shuffle the training set
+    
+    shuffleindex=np.arange(torch_x.size()[0])
+    np.random.shuffle(shuffleindex)
+
+    train_index=int(trainsize*torch_x.size()[0])
+    
+    train_x=torch_x[shuffleindex[0:train_index]]
+    train_y=torch_y[shuffleindex[0:train_index]]
+    
+    test_x=torch_x[shuffleindex[train_index:]]
+    test_y=torch_y[shuffleindex[train_index:]]
+    
+    T=epochs
+    B=100
+    NB=int(train_index/100)+1
+    N=train_index
+    #train the model
+    for epoch in range(T):
+        running_loss=0.0
+        idxminibatches = np. random . permutation (NB)
+        for k in range(NB):
+            i = idxminibatches [k]
+            idxsmp=np.arange(B*i,min(B*(i+1),N))
+            inputs = torch_x[idxsmp]
+            labels = torch_y[idxsmp]
+
+            optimizer.zero_grad()
+
+            y_pred=model(inputs)
+
+            loss=criterion(y_pred,labels)
+
+            loss.backward()
+
+            optimizer.step()
+            running_loss+=loss[0]
+            if k==NB-1:
+                print(epoch,running_loss/NB)
+                running_loss=0.0
+                
+    #assess the model on test set
+    
+    y_pred=model(test_x)
+    loss=criterion(y_pred,test_y)
+    print('MSE on test set',loss[0])
+    
+    return model
          
-  
+
+train_model('linear',1000,0.9)
+model=train_model('shallow',1000,0.9)
+
+def prediction(data,model):
+    assert isinstance(data,np.ndarray)
+    data=Variable(torch.from_numpy(data),requires_grad = False)
+    #assert data.shape[1]==19
+    #normalize data according to original dataset to fit the model
+    data=(data- dataMin) / (dataMax - dataMin)
+    newmodel=model.cpu()
+    return newmodel(data).item()
+    
 
 
-# In[10]:
-
-
-y_pred=model(torch_x)
-
-
-# In[11]:
-
-
-y_pred
-
-
-# In[12]:
-
-
-torch_y=torch_y.reshape([2938,1])
-
-
-# In[13]:
-
-
-y_pred.shape
-
-
-# In[14]:
-
-
-T=5000
-B=100
-NB=30
-N=2938
-for epoch in range(T):
-    running_loss=0.0
-    idxminibatches = np. random . permutation (NB)
-    for k in range(NB):
-        i = idxminibatches [k]
-        idxsmp=np.arange(B*i,min(B*(i+1),N))
-        inputs = torch_x[idxsmp]
-        labels = torch_y[idxsmp]
-        
-        optimizer.zero_grad()
-
-        y_pred=model(inputs)
-
-        loss=criterion(y_pred,labels)
-        
-        loss.backward()
-
-        optimizer.step()
-        running_loss+=loss[0]
-        if k==29:
-            
-            print(epoch,running_loss/30)
-            running_loss=0.0
-
-
-
-        
-
-
-            
-
-
-# In[15]:
-
-
-prediction=model(torch_x)
-
-
-# In[16]:
-
-
-prediction
-
-
-# In[18]:
-
-
-test_x=torch_x[300:600]
-test_y=torch_y[300:600]
-pred_y=model(test_x)
-loss=criterion(pred_y,test_y)
-print(loss)
-
-
-# In[ ]:
-
-
-#dataset has't been divided into training set, test set, validation set
-#
+testdata=np.array(data.iloc[2039])
+#print(testdata)
+result=prediction(testdata,model)
+print(result)
 
