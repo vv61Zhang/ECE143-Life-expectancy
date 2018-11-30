254f4e784016f68a1bfe6517ee2335209ea4b2c5
Arda-Bati <45081766+Arda-Bati@users.noreply.github.com>
2018-11-29 23:14:43 -0800

    Add files via upload

--- Project/Data_Cleaning/clean_data.py
+++ Project/Data_Cleaning/clean_data.py
@@ -2,7 +2,7 @@
 Created on Mon Nov 12 2018
 @author: Arda C. Bati
 Does data cleaning on the WHO Life expectancy data. Generates different cleaned 
-versions of the data in the directory ./output. The calss should run in the same
+versions of the data in the directory ./output. The class should run in the same
 folder/directory with the file 'Life Expectancy Data.csv', which is provided on 
 Group 5's github page. 
 The create_csvs() function will create .csv files from the variables obtained in 
@@ -25,16 +25,15 @@ class CleanData:
         which should be in the same folder with where this function is ran. Contains
         the variables given below:
         
-        NoNaN: 
+        NoNaN: (DataFrame)
             The version of the dataset with rows containing any NaN value removed.
-        NaN: 
+        NaN: (DataFrame)
             The version of the dataset with 0 values replaced with NaN
-        modified: 
+        modified:(DataFrame)
             The version of the dataset where NaN values are filled with interpolation / means.
-        feature_tables: 
+        feature_tables: (list)
             list containing seperate dataframes for each frame from the data.
-        indices: 
-            The indices of the above mentioned feature_tables list.
+
         
         param: log_level
             chooses the logging level of the initializer
@@ -44,7 +43,6 @@ class CleanData:
         self.__NaN = 0
         self.__modified = 0
         self.__feature_tables = 0
-        self.indices = 0
         
         assert isinstance(log_level,int)
         assert 0 <= log_level <= 50
@@ -62,7 +60,7 @@ class CleanData:
         df.loc[df['Status'] == 'Developing','Status'] = 1
         df.loc[df['Status'] == 'Developed','Status'] = 2
         
-        columns = list(df.columns.values)
+        #columns = list(df.columns.values)
         
         countries_years = df.groupby('Country').count()['Year']
         countries_missing_years = countries_years[countries_years != 16]
@@ -78,77 +76,24 @@ class CleanData:
         # Dropping rows containing NaN in any of their columns
         NoNaN = NaN.copy()
         NoNaN = NoNaN.dropna(0)
-         
-        # Going over each feature in the dataset, dropping rows containing NaN
-        indices = [columns[2]] + columns[4:]
-        feature_tables = [df[['Country','Year','Life expectancy ', i]].dropna(0)  for i in indices]
-        feature_tables.append(df[['Country','Year','Life expectancy ']].dropna(0))
         
-        logging.debug('The feature_tables list is in the following format.\n')
-		
-        for i in range(len(indices)):
-            logging.debug(f' {i}- { indices[i], feature_tables[i].shape}.')
-            
-        logging.debug(f" 19- ('Country, Year and Life expectancy'), {feature_tables[-1].shape}.\n")
+        feature_tables = self.__generate_tables(df)
         
         # Modifying the NaN values by interpolation and mean
-        countries = df['Country'].unique()
-        modified = NaN.copy()
+        to_modify = NaN.copy()
+        modified = self.__modify_data(to_modify,NaN)
         
-        for i in countries:
-            modified[modified['Country'] == i] = NaN[NaN['Country'] == i].interpolate(method = 'linear', limit_direction='both')
-        
-        isNaN1 = modified.isnull().values.any()
-        logging.debug(f' After interpolation, modified still contains NaN values: {isNaN1}')
-        logging.debug(f' These NaN values are the ones that could not be interpolated, the whole column for the specific country\'s feature was NaN.')
-        logging.debug(f' For this type of NaN value, we have no info, so they will be filled with the column averages of the whole data.\n')
-        
-        means = NaN.copy()
-        means = means.mean()
-        
-        for i in means.index:
-            modified[i] = modified[i].fillna(means[i])
-           
-        isNaN2 = modified.isnull().values.any()
-        logging.debug(f' After this modification checking for NaN values again:')
-        logging.debug(f' modified still contains NaN values: {isNaN2}')
-        logging.debug(f' Use create_csvs() function to create csv files from the variables in the code.\n')
- 
+        sensitivity = 3
+        modified = self.__decimal_fix(modified.copy(),sensitivity)
     
-        modified = modified.reset_index(drop=True)
-        NaN = NaN.reset_index(drop=True)
-        NoNaN = NoNaN.reset_index(drop=True)
-    
+        #Doing the final corrections on the data
+        modified, NaN, NoNaN, feature_tables = self.__final_corrections(modified.copy(), NaN.copy(), NoNaN.copy(), feature_tables)
+
         self.__NoNaN = NoNaN
         self.__NaN = NaN
         self.__modified = modified
         self.__feature_tables = feature_tables
-        self.indices = indices
-    
-    def create_csvs(self):
-        '''
-        Creates csv files from the variables of the object.
-        csv files created are:
-        output/NaN.csv
-        output/NoNaN.csv
-        output/modified.csv
-        output/feature_tables/<feature_name>.csv
-        '''
         
-        assert False == os.path.isdir('output'),'Please erase the current output directory "./output".'
-        
-        os.mkdir('output')
-        self.__NaN.to_csv('output/NaN.csv')
-        self.__NoNaN.to_csv('output/NoNaN.csv')
-        self.__modified.to_csv('output/modified.csv')
-        
-        os.mkdir('output/feature_tables')
-        for i in range(len(self.indices)):
-            self.__feature_tables[i].to_csv(f'output/feature_tables/{i}-{self.indices[i].replace("/", "-")}.csv')
-        
-        self.__feature_tables[-1].to_csv(f'output/feature_tables/19-Country_Year_LifeExpectancy.csv')
-        
-        logging.debug(f' csv files created. \n')
         
     @property
     def NoNaN(self):
@@ -165,3 +110,223 @@ class CleanData:
     @property
     def feature_tables(self):
         return self.__feature_tables.copy()
+        
+    @staticmethod
+    def __modify_data(to_modify,NaN):
+        '''
+        This is a private static method creates modifies the input DataFrame by interpolation and taking means.
+        The aim of this method is to fill all NaN values in the dataset, with reasonable values. The process
+        is explained in detail by logging statements.
+        
+        params:
+            to_modify (DataFrame)
+        
+        returns:
+            modified (DataFrame)
+        '''
+        
+        assert isinstance(to_modify,pd.DataFrame)
+        assert isinstance(NaN,pd.DataFrame)
+        
+        countries = to_modify['Country'].unique()
+        
+        for i in countries:
+            to_modify[to_modify['Country'] == i] = NaN[NaN['Country'] == i].interpolate(method = 'linear', limit_direction='both')
+        
+        isNaN1 = to_modify.isnull().values.any()
+        logging.debug(f' After interpolation, modified still contains NaN values: {isNaN1}')
+        logging.debug(f' These NaN values are the ones that could not be interpolated, the whole column for the specific country\'s feature was NaN.')
+        logging.debug(f' For this type of NaN value, we have no info, so they will be filled with the column averages of the whole data.\n')
+        
+        means = NaN.copy()
+        means = means.mean()
+        
+        for i in means.index:
+            to_modify[i] = to_modify[i].fillna(means[i])
+                     
+        isNaN2 = to_modify.isnull().values.any()
+        logging.debug(f' After this modification checking for NaN values again:')
+        logging.debug(f' modified still contains NaN values: {isNaN2}')
+        logging.debug(f' Use create_csvs() function to create csv files from the variables in the code.\n')
+        
+        modified = to_modify
+        
+        return modified
+        
+
+    @staticmethod
+    def __generate_tables(df):
+        '''
+        This is a private static method creates the feature tables for the relevant features of the dataset.
+        The logging statements state the details of the process.
+        
+        params:
+            df (DataFrame)
+        
+        returns:
+            feature_tables (list)
+        ''' 
+        
+        assert isinstance(df,pd.DataFrame)
+        
+        # Going over each feature in the dataset, dropping rows containing NaN
+        columns = list(df.columns.values)
+        indices = [columns[2]] + columns[4:]
+        feature_tables = [df[['Country','Year','Life expectancy ', i]].dropna(0)  for i in indices]
+        feature_tables.append(df[['Country','Year','Life expectancy ']].dropna(0))
+        
+        logging.debug('The feature_tables list is in the following format.\n')
+		
+        for i in range(len(indices)):
+            logging.debug(f' {i}- { indices[i], feature_tables[i].shape}.')
+            
+        logging.debug(f" 19- ('Country, Year and Life expectancy'), {feature_tables[-1].shape}.\n")
+        
+        return feature_tables
+        
+        
+    @staticmethod
+    def __final_corrections(modified, NaN, NoNaN, feature_tables):
+        '''
+        This is a private static method which does the final corrections on the CleanData variables.
+        It takes the below variables as input, and returns their final corrected versions.
+        
+        params:
+            modified, (DataFrame)
+            NaN (DataFrame)
+            NoNaN (DataFrame)
+            feature_tables (list)
+        
+        returns:
+            modified, (DataFrame)
+            NaN (DataFrame)
+            NoNaN (DataFrame)
+            feature_tables (list)
+        '''
+        
+        assert isinstance(modified,pd.DataFrame)
+        assert isinstance(NaN,pd.DataFrame)
+        assert isinstance(NoNaN,pd.DataFrame)
+        assert isinstance(feature_tables,list)
+        
+        #Fixing indexing issues
+        columns = list(modified.columns.values)
+        modified = modified.reset_index(drop=True)
+        NaN = NaN.reset_index(drop=True)
+        NoNaN = NoNaN.reset_index(drop=True)
+        
+        columns2 = columns
+        columns2.remove('Country')
+        
+        #Fixing floating point issues
+        modified[columns2] = modified[columns2].astype(np.float32)
+        NaN[columns2] = NaN[columns2].astype(np.float32)
+        NoNaN[columns2] = NoNaN[columns2].astype(np.float32)
+        
+        for table in feature_tables:
+            columns_t = list(table.columns.values)
+            columns_t.remove('Country')
+            table[columns_t] = table[columns_t].astype(np.float32)
+            table.reset_index(drop=True)
+        
+        return modified, NaN, NoNaN, feature_tables
+    
+    @staticmethod
+    def __decimal_fix(df,sensitivity):
+        '''
+        This is a private static method is used to correct the decimal point errors in the dataset.
+        We realized that there are sudden spikes in data through the years, which is mainly caused
+        by wrongly placed decimal points. This method tries to fix this issue as much as possible.
+        
+        sensitivity is an integer value in the open interval (1, 10). It determines the sensitivity
+        for detecting the sudden spikes in the dataset, which are caused by decimal point errors.
+        
+        input df should contain no 0, NaN, or negative values
+        
+        params:
+            df (DataFrame)
+            sensitivity (int)
+        
+        returns:
+            df_fixed (DataFrame)
+        '''
+        
+        import numpy as np
+        
+        assert isinstance(sensitivity,int)
+        assert 1 < sensitivity < 10
+
+        assert isinstance(df,pd.DataFrame)
+        df = df.replace(0,np.NaN)
+        assert df.isnull().values.any() == False
+        assert all(df > 0)
+        
+        columns = list(df.columns.values)
+        indices = [columns[2]] + columns[4:]
+        
+        countries = df['Country'].unique()
+        
+        means_c = []
+        for country in countries:
+            means_c.append(df[df['Country'] == country].mean())
+            
+        indices.remove('Status')
+            
+        for count,country in enumerate(countries):
+            
+            for index in indices:
+               a2 = df.loc[df['Country'] == country,index]
+               
+               for loc1,point1 in enumerate(a2):
+                   
+                   key1 = df.index[df['Country']==country].tolist()
+                   key1 = key1[loc1]
+                   point1 = df.at[key1,index]
+                   
+                   if loc1  == 0:
+                       point2 = means_c[count]
+                       point2 = point2[index]
+                   else:
+                       key2 = df.index[df['Country']==country].tolist()
+                       key2 = key2[loc1-1]
+                       point2 = df.at[key2,index]
+                       
+                   if (((point1 / (point2)) < 1/sensitivity)):
+                       while ((point1 / (point2)) < 1/sensitivity):
+                           point1 = point1*10
+                   elif ((point1 / (point2)) > sensitivity):
+                       while ((point1 / (point2)) > sensitivity):
+                           point1 = point1/10
+                           
+                   df.at[key1,index] = point1
+        df_fixed = df          
+        return df_fixed      
+        
+    
+    def create_csvs(self):
+        '''
+        Creates csv files from the variables of the object.
+        csv files created are:
+        output/NaN.csv
+        output/NoNaN.csv
+        output/modified.csv
+        output/feature_tables/<feature_name>.csv
+        '''
+        
+        columns = list(self.NaN.columns.values)
+        indices = [columns[2]] + columns[4:]
+        
+        assert False == os.path.isdir('output'),'Please erase the current output directory "./output".'
+        
+        os.mkdir('output')
+        self.__NaN.to_csv('output/NaN.csv')
+        self.__NoNaN.to_csv('output/NoNaN.csv')
+        self.__modified.to_csv('output/modified.csv')
+        
+        os.mkdir('output/feature_tables')
+        for i in range(len(indices)):
+            self.__feature_tables[i].to_csv(f'output/feature_tables/{i}-{indices[i].replace("/", "-")}.csv')
+        
+        self.__feature_tables[-1].to_csv(f'output/feature_tables/19-Country_Year_LifeExpectancy.csv')
+        
+        logging.debug(f' csv files created. \n')
